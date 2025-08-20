f8c1587fbefbba9660122f34d430837e5b522f68
владимир кочкин <vladsky1978@gmail.com>
2025-08-21 01:59:03 +0300

    162 продолжаю адаптацию

--- index-header-desktop-keys.js
+++ index-header-desktop-keys.js
@@ -9,46 +9,52 @@ const headerKeys = {
   promoLeft: {
     1920: { top: 80, left: 100, width: 450, height: 450 },
     1440: { top: 80, left: 20, width: 370, height: 370 },
-    1280: { top: 80, left: 20, width: 340, height: 340 },
-    960: { top: 430, left: 20, width: 300, height: 300 },
-    720: { top: 430, left: 320, width: 300, height: 300 },
+    1281: { top: 80, left: 20, width: 340, height: 340 },
+    1280: { top: 380, left: 50, width: 300, height: 300 },
+    961: { top: 380, left: 20, width: 300, height: 300 },
+    720: { top: 430, left: 30, width: 200, height: 200 },
   },
   promoRight: {
     1920: { top: 80, left: 1360, width: 450, height: 450 },
     1440: { top: 80, left: 1030, width: 370, height: 370 },
-    1280: { top: 80, left: 910, width: 340, height: 340 },
-    960: { top: 430, left: 640, width: 300, height: 300 },
-    720: { top: 430, left: 320, width: 300, height: 300 },
+    1281: { top: 80, left: 910, width: 340, height: 340 },
+    1280: { top: 380, left: 920, width: 300, height: 300 },
+    961: { top: 380, left: 640, width: 300, height: 300 },
+    720: { top: 430, left: 260, width: 200, height: 200 },
   },
 
   // Нижние рекламные блоки под фильтрами (каждый блок отдельно)
   promoRow1: {
     1920: { top: 555, left: 320, width: 300, height: 300 },
     1440: { top: 500, left: 100, width: 300, height: 300 },
-    1280: { top: 430, left: 20, width: 300, height: 300 },
-    960: { top: 430, left: 330, width: 300, height: 300 },
-    720: { top: 430, left: 320, width: 300, height: 300 },
+    1281: { top: 430, left: 20, width: 300, height: 300 },
+    1280: { top: 380, left: 490, width: 300, height: 300 },
+    961: { top: 380, left: 330, width: 300, height: 300 },
+    720: { top: 640, left: 30, width: 200, height: 200 },
   },
   promoRow2: {
     1920: { top: 555, left: 640, width: 300, height: 300 },
     1440: { top: 500, left: 420, width: 300, height: 300 },
-    1280: { top: 430, left: 330, width: 300, height: 300 },
-    960: { top: 740, left: 20, width: 300, height: 300 },
-    720: { top: 430, left: 640, width: 300, height: 300 },
+    1281: { top: 430, left: 330, width: 300, height: 300 },
+    1280: { top: 690, left: 50, width: 300, height: 300 },
+    961: { top: 690, left: 20, width: 300, height: 300 },
+    720: { top: 640, left: 260, width: 200, height: 200 },
   },
   promoRow3: {
     1920: { top: 555, left: 960, width: 300, height: 300 },
     1440: { top: 500, left: 740, width: 300, height: 300 },
-    1280: { top: 430, left: 640, width: 300, height: 300 },
-    960: { top: 740, left: 330, width: 300, height: 300 },
-    720: { top: 430, left: 960, width: 300, height: 300 },
+    1281: { top: 430, left: 640, width: 300, height: 300 },
+    1280: { top: 690, left: 490, width: 300, height: 300 },
+    961: { top: 690, left: 330, width: 300, height: 300 },
+    720: { top: 850, left: 30, width: 200, height: 200 },
   },
   promoRow4: {
     1920: { top: 555, left: 1280, width: 300, height: 300 },
     1440: { top: 500, left: 1060, width: 300, height: 300 },
-    1280: { top: 430, left: 950, width: 300, height: 300 },
-    960: { top: 740, left: 640, width: 300, height: 300 },
-    720: { top: 430, left: 1280, width: 300, height: 300 },
+    1281: { top: 430, left: 950, width: 300, height: 300 },
+    1280: { top: 690, left: 920, width: 300, height: 300 },
+    961: { top: 690, left: 640, width: 300, height: 300 },
+    720: { top: 850, left: 260, width: 200, height: 200 },
   },
 
   // Заголовок
@@ -57,7 +63,7 @@ const headerKeys = {
     1440: { top: 68, left: 590, fontSize: 48, width: 350 },
     1280: { top: 68, left: 510, fontSize: 48, width: 350 },
     960: { top: 215, left: 100, fontSize: 48, width: 350 },
-    720: { top: 68, left: 830, fontSize: 48, width: 350 },
+    720: { top: 325, left: 10, fontSize: 40, width: 470 },
   },
   // Подпись под заголовком
   pageSubtitle: {
@@ -65,7 +71,7 @@ const headerKeys = {
     1440: { top: 180, left: 590, fontSize: 27 },
     1280: { top: 180, left: 510, fontSize: 24 },
     960: { top: 385, left: 100, fontSize: 22 },
-    720: { top: 180, left: 830, fontSize: 32 },
+    720: { top: 385, left: 100, fontSize: 22 },
   },
 
   // Кнопка поиска
@@ -74,7 +80,7 @@ const headerKeys = {
     1440: { top: 380, left: 420, width: 590, height: 35 },
     1280: { top: 380, left: 365, width: 540, height: 35 },
     960: { top: 380, left: 470, width: 350, height: 40 },
-    720: { top: 380, left: 600, width: 300, height: 35 },
+    720: { top: 278, left: 340, width: 140, height: 40 },
   },
 
   // Кнопки (каждая отдельная)
@@ -83,28 +89,28 @@ const headerKeys = {
     1920: { top: 28, left: 50, width: 80, height: 40 },
     1440: { top: 28, left: 50, width: 80, height: 40 },
     1280: { top: 28, left: 50, width: 80, height: 40 },
-    720: { top: 28, left: 10, width: 80, height: 40 },
+    720: { top: 28, left: 340, width: 140, height: 40 },
   },
   authAdmin: {
     // Кнопка АДМИНКА
     1920: { top: 28, left: 150, width: 90, height: 40 },
     1440: { top: 28, left: 150, width: 90, height: 40 },
     1280: { top: 28, left: 150, width: 90, height: 40 },
-    720: { top: 28, left: 95, width: 90, height: 40 },
+    720: { top: 78, left: 340, width: 140, height: 40 },
   },
   authAddService: {
     // Кнопка ДОБАВИТЬ УСЛУГУ
     1920: { top: 28, left: 255, width: 140, height: 40 },
     1440: { top: 28, left: 255, width: 140, height: 40 },
     1280: { top: 28, left: 255, width: 140, height: 40 },
-    720: { top: 28, left: 190, width: 140, height: 40 },
+    720: { top: 128, left: 340, width: 140, height: 40 },
   },
   authProfile: {
     // Кнопка ЛИЧНЫЙ КАБИНЕТ
     1920: { top: 28, left: 410, width: 133, height: 40 },
     1440: { top: 28, left: 410, width: 133, height: 40 },
     1280: { top: 28, left: 410, width: 133, height: 40 },
-    720: { top: 28, left: 335, width: 133, height: 40 },
+    720: { top: 178, left: 340, width: 140, height: 40 },
   },
 
   // Поля фильтров (каждое поле отдельно)
@@ -114,7 +120,7 @@ const headerKeys = {
     1440: { top: 230, left: 410, width: 300, height: 40 },
     1280: { top: 230, left: 365, width: 265, height: 40 },
     960: { top: 80, left: 470, width: 350, height: 40 },
-    720: { top: 230, left: 600, width: 350, height: 40 },
+    720: { top: 28, left: 110, width: 220, height: 40 },
   },
   filter2: {
     // Поле ГОРОД
@@ -122,7 +128,7 @@ const headerKeys = {
     1440: { top: 230, left: 715, width: 300, height: 40 },
     1280: { top: 230, left: 640, width: 265, height: 40 },
     960: { top: 130, left: 470, width: 350, height: 40 },
-    720: { top: 230, left: 955, width: 350, height: 40 },
+    720: { top: 78, left: 110, width: 220, height: 40 },
   },
 
   filter3: {
@@ -131,7 +137,7 @@ const headerKeys = {
     1440: { top: 280, left: 410, width: 300, height: 40 },
     1280: { top: 280, left: 365, width: 265, height: 40 },
     960: { top: 180, left: 470, width: 350, height: 40 },
-    720: { top: 280, left: 600, width: 350, height: 40 },
+    720: { top: 128, left: 110, width: 220, height: 40 },
   },
   filter4: {
     // Поле ВИД
@@ -139,7 +145,7 @@ const headerKeys = {
     1440: { top: 280, left: 715, width: 300, height: 40 },
     1280: { top: 280, left: 640, width: 265, height: 40 },
     960: { top: 230, left: 470, width: 350, height: 40 },
-    720: { top: 280, left: 955, width: 350, height: 40 },
+    720: { top: 178, left: 110, width: 220, height: 40 },
   },
 
   filter5: {
@@ -148,7 +154,7 @@ const headerKeys = {
     1440: { top: 330, left: 410, width: 300, height: 40 },
     1280: { top: 330, left: 365, width: 265, height: 40 },
     960: { top: 280, left: 470, width: 350, height: 40 },
-    720: { top: 330, left: 600, width: 350, height: 40 },
+    720: { top: 228, left: 110, width: 220, height: 40 },
   },
   filter6: {
     // Поле ИМЯ
@@ -156,7 +162,7 @@ const headerKeys = {
     1440: { top: 330, left: 715, width: 300, height: 40 },
     1280: { top: 330, left: 640, width: 265, height: 40 },
     960: { top: 330, left: 470, width: 350, height: 40 },
-    720: { top: 330, left: 955, width: 350, height: 40 },
+    720: { top: 278, left: 110, width: 220, height: 40 },
   },
 };
 
