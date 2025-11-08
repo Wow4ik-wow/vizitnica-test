9327358914a6efc76d6671ceee3c0aa9fb21a12c
Wow4ik <vladsky1978@gmail.com>
2025-11-09 01:51:08 +0200

    ТГ 04

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const API_USER_URL =
   "https://script.google.com/macros/s/AKfycbzpraBNAzlF_oqYIDLYVjczKdY6Ui32qJNwY37HGSj6vtPs9pXseJYqG3oLAr28iZ0c/exec";
 let currentUser = null;
 
+alert("1 - Переменные объявлены");
+
 // Проверяем, открыт ли сайт внутри Telegram WebApp
 // === Проверяем, открыт ли сайт внутри Telegram WebApp ===
 let isTelegramWebApp = false;
