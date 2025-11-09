9220b764bef5a6dc468f038d903a6a1d229d056f
Wow4ik <vladsky1978@gmail.com>
2025-11-09 02:05:11 +0200

    ТГ 10

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ let currentUser = null;
 let allServices = [];
 
 alert("index.js выполняется!");
+
+// TG логика
+let isTelegramWebApp = false;
+let tgUser = null;
+
+function isReallyTelegramWebApp() {
+  return (
+    typeof window.Telegram !== "undefined" &&
+    window.Telegram.WebApp &&
+    window.Telegram.WebApp.initData &&
+    window.Telegram.WebApp.platform &&
+    window.Telegram.WebApp.platform !== "unknown"
+  );
+}
+
+alert("TG логика добавлена!");
