fc06fee68959e615d55dff3e0288e720673e4aee
Wow4ik <vladsky1978@gmail.com>
2025-11-09 02:26:45 +0200

    ТГ 14

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ let tgUser = null;
 alert("1 - Переменные TG объявлены");
 
 function isReallyTelegramWebApp() {
-  alert("2 - Проверка TG началась");
-  return (
-    typeof window.Telegram !== "undefined" &&
-    window.Telegram.WebApp &&
-    window.Telegram.WebApp.initData &&
-    window.Telegram.WebApp.platform &&
-    window.Telegram.WebApp.platform !== "unknown"
-  );
+  alert("2.1 - Проверка window.Telegram");
+  if (typeof window.Telegram === "undefined") return false;
+  alert("2.2 - Проверка Telegram.WebApp");
+  if (!window.Telegram.WebApp) return false;
+  alert("2.3 - Проверка initData");
+  if (!window.Telegram.WebApp.initData) return false;
+  alert("2.4 - Проверка platform");
+  if (!window.Telegram.WebApp.platform) return false;
+  alert("2.5 - Проверка platform !== unknown");
+  if (window.Telegram.WebApp.platform === "unknown") return false;
+  
+  return true;
 }
 
 alert("3 - Функция создана");
