bfce9d844eaaa6a7b75bd68bc896db6e67492983
Wow4ik <vladsky1978@gmail.com>
2025-11-09 02:00:09 +0200

    ТГ 08

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const apiUrl = "https://raw.githubusercontent.com/Wow4ik-wow/vizitnica/master/data.json";
-const API_USER_URL = "https://script.google.com/macros/s/AKfycbzpraBNAzlF_oqYIDLYVjczKdY6Ui32qJNwY37HGSj6vtPs9pXseJYqG3oLAr28iZ0c/exec";
+// Минимальный тестовый код
+const apiUrl = "test";
+const API_USER_URL = "test"; 
 let currentUser = null;
+let allServices = [];
 
-alert("Базовый код работает!");
+alert("index.js выполняется!");
